f6304dfaa4a04db3a3cfbfc1dcea7f7edf3ee859
Alexander <alexander.martinez@lapatota.cl>
2022-10-10 15:24:20 -0300

    Manejar un objeto como estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
+import { CounterBy } from './bases/CounetBy'
 import { Counter } from './bases/Counter'
 
 function App() {
   return (
     <>
-      <h1>React</h1>
-      <hr />
-
       <Counter initialValue={15} />
+      <CounterBy />
     </>
   )
 }
