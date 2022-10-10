d9061755885a95f28781f629bacaaaa7d9d84ffd
Alexander <alexander.martinez@lapatota.cl>
2022-10-10 15:48:45 -0300

    useEffect - CounterEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { CounterBy } from './bases/CounetBy'
 import { Counter } from './bases/Counter'
+import { CounterEffect } from './bases/CounterEffect'
 
 function App() {
   return (
     <>
       <Counter initialValue={15} />
       <CounterBy />
+      <CounterEffect />
     </>
   )
 }
