f0f57364781ce8b62176c1ab1ed365c2907005e1
Alexander <alexander.martinez@lapatota.cl>
2022-10-10 22:47:38 -0300

    Separar acciones, interfaces y reducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import { CounterBy } from './bases/CounetBy'
 import { Counter } from './bases/Counter'
 import { CounterEffect } from './bases/CounterEffect'
 import { CounterHook } from './bases/CounterHook'
-import { CounterReducerComponent } from './bases/CounterReducer'
+import { CounterReducerComponent } from './counter-reducer/CounterReducer'
+// import { CounterReducerComponent } from './bases/CounterReducer'
 
 function App() {
   return (
     <>
-      <CounterReducerComponent />
       <Counter initialValue={15} />
       <CounterBy />
       <CounterEffect />
       <CounterHook />
+      <CounterReducerComponent />
     </>
   )
 }
